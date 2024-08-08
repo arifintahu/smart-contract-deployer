f20b22182feec3b9f70334ae6ed70d6d0b55f5fb
Miftahul Arifin <miftahul97@gmail.com>
2024-08-08 20:50:16 +0700

    chore: add custom theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,17 @@
-import '@/styles/globals.css'
+import '@/lib/styles/globals.css'
 import type { AppProps } from 'next/app'
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+import { fonts } from '@/lib/styles/theme/fonts'
+import { config } from '@/lib/styles/theme/config'
+
+const customTheme = extendTheme({
+  fonts,
+  config,
+})
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={customTheme}>
       <Component {...pageProps} />
     </ChakraProvider>
   )
