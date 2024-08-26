9eb9861f2cf4d2b570934fe02dbb9505a1660c2e
Miftahul Arifin <miftahul97@gmail.com>
2024-08-26 17:16:11 +0700

    fix: update local storage validation window

--- src/lib/utils/storage.ts
+++ src/lib/utils/storage.ts
@@ -2,6 +2,9 @@ export const getStorageKey = (key: string): string => `deployer-app-${key}`
 
 export const setItem = <T>(key: string, value: T): void => {
   try {
+    if (typeof window === 'undefined') {
+      return
+    }
     window.localStorage.setItem(getStorageKey(key), JSON.stringify(value))
   } catch (error) {
     // eslint-disable-next-line no-console
@@ -11,6 +14,9 @@ export const setItem = <T>(key: string, value: T): void => {
 
 export const getItem = <T>(key: string, defaultValue: T): T => {
   try {
+    if (typeof window === 'undefined') {
+      return defaultValue
+    }
     const value = window.localStorage.getItem(getStorageKey(key))
     if (value) {
       return JSON.parse(value) as T
